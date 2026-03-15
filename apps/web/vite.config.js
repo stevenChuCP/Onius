f6a85961d87fc0488b3db710144225bb05081ade
Steven Chu <stevenchu@Stevens-MacBook-Pro.local>
2026-03-15 23:14:15 +0800

    add: Github Action for deploying

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -3,6 +3,11 @@ import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+    // Tell Vite where to find the app.
+    // If you are deploying to GitHub Pages, vite will set path to '/Onius/',
+    // else it will set path to '/'.
+    // This is used for both development and production.
+    base: process.env.GITHUB_PAGES ? '/Onius/' : '/',
     plugins: [
         wasm(),
         topLevelAwait()
